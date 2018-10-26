fe0f7075f37368b4222215b57aacbe93b2c1ecbd
kevinismantara <ismantara.kevin@yahoo.com>
2018-10-26 01:38:03 -0700

    add username and password as required field

--- resources/js/components/SignIn.js
+++ resources/js/components/SignIn.js
@@ -125,13 +125,14 @@ const LoginForm = () => {
                     </Header>
                     <Form size='large'>
                     <Segment stacked>
-                        <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                        <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' required/>
                         <Form.Input
                         fluid
                         icon='lock'
                         iconPosition='left'
                         placeholder='Password'
                         type='password'
+                        required
                         />
                         <Button color='teal' fluid size='large'>
                         Login
