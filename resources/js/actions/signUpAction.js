42dc81f2717e44ca27ea18a7f6a93947929613e9
kevinismantara <ismantara.kevin@yahoo.com>
2018-11-01 17:55:55 -0700

    commit signup form

--- resources/js/actions/signUpAction.js
+++ resources/js/actions/signUpAction.js
@@ -1,3 +1,10 @@
-export const registerUser = (firstName, lastName, gender, email, password) => {
-    
+export const registerUser = (firstName, lastName, gender, email, password, retypedpassword) => {
+    return (dispatch) => {
+        console.log("first Name", firstName)
+        console.log("last Name", lastName)
+        console.log("email", email)
+        console.log("gender", gender)
+        console.log("password", password)
+        console.log("retypedpass", retypedpassword)
+    }
 }
