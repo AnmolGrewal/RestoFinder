641939b9610e0532c51dba16b6495081cf153352
kevinismantara <ismantara.kevin@yahoo.com>
2018-11-01 19:06:28 -0700

    change connection url

--- resources/js/actions/signUpAction.js
+++ resources/js/actions/signUpAction.js
@@ -3,12 +3,13 @@ import axios from "axios";
 export const registerUser = (firstName, lastName, gender, email, password) => {
     return (dispatch) => {
         let params = new FormData()
+        let currenturl = window.location.hostname
         params.append('first_name', firstName)
         params.append('last_name', lastName)
         params.append('email', email)
         params.append('gender', gender)
         params.append('password', password)
-        let url = 'http://localhost:8000/database/user/createUser.php'
+        let url = `http://${currenturl}:80/database/user/createUser.php`
         axios.post(url, params)
             .then(response => console.log(response))
     
